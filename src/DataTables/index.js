db31a1b338da41ea2fc6590fdafd1e6b49523e70
PuteauxTheo <91119961+PuteauxTheo@users.noreply.github.com>
2022-09-11 14:01:43 +0200

    add css class

--- src/DataTables/index.js
+++ src/DataTables/index.js
@@ -1,11 +1,27 @@
 import "./DataTable.css";
+import LengthEntries from "./LengthEntries";
+import Search from "./Search";
+import Table from "./Table";
+import ShowEntries from "./ShowEntries";
+import Pagination from "./Pagination";
 
 
-export default function DataTable(){
+
+export default function DataTable() {
     return (
         <div>
+            <div className="top-table">
+                <LengthEntries />
+                <Search />
+            </div>
 
+            <Table />
+
+            <div className="bot-table">
+                <ShowEntries />
+                <Pagination />
+            </div>
         </div>
     )
-    
+
 }
