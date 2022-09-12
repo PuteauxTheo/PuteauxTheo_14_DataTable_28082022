a8e58e6431a270f9800fff8f71864f6769144154
Theo Puteaux <theo.ptx@laposte.net>
2022-09-12 13:20:27 +0200

    add label for data

--- src/exempleData.js
+++ src/exempleData.js
@@ -36,3 +36,15 @@ export const exempleData = [
         zipCode: "72000"
     }
 ]
+
+export const exempleLabels = [
+    { text: "First Name", value: "firstName" },
+    { text: "Last Name", value: "lastName" },
+    { text: "Start Date", value: "startDate" },
+    { text: "Department", value: "department" },
+    { text: "Date of Birth", value: "dateOfBirth" },
+    { text: "Street", value: "street" },
+    { text: "City", value: "city" },
+    { text: "State", value: "state" },
+    { text: "Zip Code", value: "zipCode" },
+];
