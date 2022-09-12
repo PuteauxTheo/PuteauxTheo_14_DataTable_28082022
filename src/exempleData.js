4312616f3a70df792ba0edfa126272881973bd72
Theo Puteaux <theo.ptx@laposte.net>
2022-09-12 12:22:12 +0200

    add exemple data for test

---
+++ src/exempleData.js
@@ -0,0 +1,38 @@
+export const exempleData = [
+    {
+        idEmployee: 0,
+        firstName: "Paul",
+        lastName: "Georges",
+        dateOfBirth: "06/18/2000",
+        startDate: "09/01/2022",
+        department: "Engineering",
+        street: "1 rue le maire",
+        city: "Nantes",
+        state: "Pays de la loire",
+        zipCode: "44000"
+    },
+    {
+        idEmployee: 1,
+        firstName: "Armand",
+        lastName: "Thierry",
+        dateOfBirth: "02/09/1998",
+        startDate: "10/21/2022",
+        department: "Engineering",
+        street: "2 rue le maire",
+        city: "Paris",
+        state: "Ile de France",
+        zipCode: "75000"
+    },
+    {
+        idEmployee: 2,
+        firstName: "Tony",
+        lastName: "Parker",
+        dateOfBirth: "11/18/1987",
+        startDate: "01/07/2022",
+        department: "Engineering",
+        street: "13 rue le maire",
+        city: "Le Mans",
+        state: "Pays de la loire",
+        zipCode: "72000"
+    }
+]
