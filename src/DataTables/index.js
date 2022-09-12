89ba3c8e95b06c5b6395e987a178c0f5dc301c2a
Theo Puteaux <theo.ptx@laposte.net>
2022-09-12 13:22:16 +0200

    add prop for component

--- src/DataTables/index.js
+++ src/DataTables/index.js
@@ -1,4 +1,5 @@
 import "./DataTable.css";
+import { useState } from "react";
 import LengthEntries from "./LengthEntries";
 import Search from "./Search";
 import Table from "./Table";
@@ -7,7 +8,14 @@ import Pagination from "./Pagination";
 
 
 
-export default function DataTable() {
+export default function DataTable({ labels, data }) {
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [entriesShown, setEntriesShown] = useState(10);
+
+    const minShow = currentPage === 1 ? 1 : (currentPage - 1) * entriesShown + 1;
+    const maxShow = currentPage * entriesShown < data.length ? currentPage * entriesShown : data.length ;
+
     return (
         <div>
             <div className="top-table">
@@ -15,10 +23,10 @@ export default function DataTable() {
                 <Search />
             </div>
 
-            <Table />
+            <Table labels={labels} data={data}/>
 
             <div className="bot-table">
-                <ShowEntries />
+                <ShowEntries minShow={minShow} maxShow={maxShow} totalEntries={data.length}/>
                 <Pagination />
             </div>
         </div>
