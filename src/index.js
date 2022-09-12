44915e0b090b28f1bcb6cd269aa6d7bbc601723a
Theo Puteaux <theo.ptx@laposte.net>
2022-09-12 13:20:11 +0200

    add data in prop DataTable

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import DataTable from './DataTables/index.js';
+import { exempleLabels, exempleData } from './exempleData.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DataTable/>
+    <DataTable labels={exempleLabels} data={exempleData}/>
   </React.StrictMode>
 );
