079acc11e1a816e0c72ece376e72858814e9f716
Theo Puteaux <theo.ptx@laposte.net>
2022-09-13 17:41:51 +0200

    add function handleSort for manipulate order ascending and descending

--- src/DataTables/index.js
+++ src/DataTables/index.js
@@ -13,6 +13,21 @@ export default function DataTable({ labels, data }) {
     const [currentPage, setCurrentPage] = useState(1);
     const [entriesShown, setEntriesShown] = useState(10);
 
+    const [sort, setSort] = useState({ column : "" , isAsc: true})
+
+    const handleSort = (label) => {
+        if(sort.column === label){
+            setSort({
+                ...sort,
+                isAsc: !sort.isAsc
+            })
+        }else{
+            setSort({
+                column: label,
+                isAsc: false
+            })
+        }
+    }
 
     const minShow = currentPage === 1 ? 1 : (currentPage - 1) * entriesShown + 1;
     const maxShow = currentPage * entriesShown < data.length ? currentPage * entriesShown : data.length ;
@@ -23,7 +38,7 @@ export default function DataTable({ labels, data }) {
                 <Search />
             </div>
 
-            <Table labels={labels} data={data} minShow={minShow} max={maxShow} />
+            <Table labels={labels} data={data} minShow={minShow} maxShow={maxShow} sort={sort} handleSort={handleSort}/>
 
             <div className="bot-table">
                 <ShowEntries minShow={minShow} maxShow={maxShow} totalEntries={data.length}/>
