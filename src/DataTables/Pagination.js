d1abf73860af0b2491c6eae126e63a8f88398042
Theo Puteaux <theo.ptx@laposte.net>
2022-09-13 13:47:31 +0200

    fix pagination issue

--- src/DataTables/Pagination.js
+++ src/DataTables/Pagination.js
@@ -1,9 +1,31 @@
-export default function Pagination() {
+export default function Pagination({ currentPage, handleClick, totalEntries, entriesShown}) {
+
+    const nbPage = Math.ceil(totalEntries / entriesShown);
+    /* Create Array with nbPage Length for simulate navigation */
+    const nbPageArray = new Array(nbPage).fill(0);
+
+    const handlePreviousPage = () => {
+        if(currentPage > 1) handleClick(currentPage - 1 ) 
+
+    }
+
+    const handleNextPage = () => {
+        if(currentPage < nbPage) handleClick(currentPage + 1)
+    }
+
     return (
         <div className="pagination">
-            <p> Previous </p>
-            <span>1</span>
-            <p>Next</p>
+            <button type="button" className={currentPage === 1 ? "pagination-btn disabled" : "pagination-btn"} onClick={handlePreviousPage}> Previous </button>
+            {
+                nbPageArray.map( (el, index) => (
+
+                    <button type="button" onClick={() => handleClick(index+1)} className={currentPage === index + 1 ? "pagination-btn active" : "pagination-btn"}>
+                        {index+1}
+                    </button>
+        
+                ))
+            }
+            <button type="button" className={currentPage === nbPage ? "pagination-btn disabled" : "pagination-btn"} onClick={handleNextPage}>Next</button>
         </div>
     )
 }
