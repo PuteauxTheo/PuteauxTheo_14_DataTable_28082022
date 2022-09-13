316b64e15533ade8783d02a95bad5b17333bfd2a
Theo Puteaux <theo.ptx@laposte.net>
2022-09-13 13:46:43 +0200

    add new data

--- src/exempleData.js
+++ src/exempleData.js
@@ -34,6 +34,114 @@ export const exempleData = [
         city: "Le Mans",
         state: "Pays de la loire",
         zipCode: "72000"
+    },
+    {
+        idEmployee: 3,
+        firstName: "Paul",
+        lastName: "Georges",
+        dateOfBirth: "06/18/2000",
+        startDate: "09/01/2022",
+        department: "Engineering",
+        street: "1 rue le maire",
+        city: "Nantes",
+        state: "Pays de la loire",
+        zipCode: "44000"
+    },
+    {
+        idEmployee: 4,
+        firstName: "Armand",
+        lastName: "Thierry",
+        dateOfBirth: "02/09/1998",
+        startDate: "10/21/2022",
+        department: "Engineering",
+        street: "2 rue le maire",
+        city: "Paris",
+        state: "Ile de France",
+        zipCode: "75000"
+    },
+    {
+        idEmployee: 5,
+        firstName: "Tony",
+        lastName: "Parker",
+        dateOfBirth: "11/18/1987",
+        startDate: "01/07/2022",
+        department: "Engineering",
+        street: "13 rue le maire",
+        city: "Le Mans",
+        state: "Pays de la loire",
+        zipCode: "72000"
+    },
+    {
+        idEmployee: 6,
+        firstName: "Paul",
+        lastName: "Georges",
+        dateOfBirth: "06/18/2000",
+        startDate: "09/01/2022",
+        department: "Engineering",
+        street: "1 rue le maire",
+        city: "Nantes",
+        state: "Pays de la loire",
+        zipCode: "44000"
+    },
+    {
+        idEmployee: 7,
+        firstName: "Armand",
+        lastName: "Thierry",
+        dateOfBirth: "02/09/1998",
+        startDate: "10/21/2022",
+        department: "Engineering",
+        street: "2 rue le maire",
+        city: "Paris",
+        state: "Ile de France",
+        zipCode: "75000"
+    },
+    {
+        idEmployee: 8,
+        firstName: "Tony",
+        lastName: "Parker",
+        dateOfBirth: "11/18/1987",
+        startDate: "01/07/2022",
+        department: "Engineering",
+        street: "13 rue le maire",
+        city: "Le Mans",
+        state: "Pays de la loire",
+        zipCode: "72000"
+    },
+    {
+        idEmployee: 9,
+        firstName: "Paul",
+        lastName: "Georges",
+        dateOfBirth: "06/18/2000",
+        startDate: "09/01/2022",
+        department: "Engineering",
+        street: "1 rue le maire",
+        city: "Nantes",
+        state: "Pays de la loire",
+        zipCode: "44000"
+    },
+    {
+        idEmployee: 10,
+        firstName: "Armand",
+        lastName: "Thierry",
+        dateOfBirth: "02/09/1998",
+        startDate: "10/21/2022",
+        department: "Engineering",
+        street: "2 rue le maire",
+        city: "Paris",
+        state: "Ile de France",
+        zipCode: "75000"
+    },
+    {
+        idEmployee: 11,
+        firstName: "Tony",
+        lastName: "Parker",
+        dateOfBirth: "11/18/1987",
+        startDate: "01/07/2022",
+        department: "Engineering",
+        street: "13 rue le maire",
+        city: "Le Mans",
+        state: "Pays de la loire",
+        zipCode: "72000"
     }
 ]
 
