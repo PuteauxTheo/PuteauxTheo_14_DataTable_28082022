6441b9652d288ae8ba729d50d5c64ad2923b5745
Theo Puteaux <theo.ptx@laposte.net>
2022-09-13 13:48:47 +0200

    add function for save number selected

--- src/DataTables/LengthEntries.js
+++ src/DataTables/LengthEntries.js
@@ -1,9 +1,14 @@
-export default function LengthEntries() {
+export default function LengthEntries({ value, handleChange }) {
+
+    const handleEntriesShown = (e) => {
+        handleChange(e.target.value)
+    }
+
     return (
         <div className="dataTable-length">
             <label>
                 <p>Show</p>
-                <select>
+                <select value={value} onChange={e => handleEntriesShown(e)}>
                     <option value={10}>10</option>
                     <option value={20}>20</option>
                 </select>
