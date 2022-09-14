2b79b0674b3f9fd67a65b8ed522c43eabfb0dea7
Theo Puteaux <theo.ptx@laposte.net>
2022-09-14 13:43:20 +0200

    add sorting Data, and prop for component

--- src/DataTables/index.js
+++ src/DataTables/index.js
@@ -10,8 +10,12 @@ import Pagination from "./Pagination";
 
 export default function DataTable({ labels, data }) {
 
+    const initialState = data;
+
     const [currentPage, setCurrentPage] = useState(1);
     const [entriesShown, setEntriesShown] = useState(10);
+    const [sortedData, setSortedData] = useState(initialState);
+    const [isSearching, setIsSearching] = useState(false);
 
     const [sort, setSort] = useState({ column : "" , isAsc: true})
 
@@ -20,28 +24,63 @@ export default function DataTable({ labels, data }) {
             setSort({
                 ...sort,
                 isAsc: !sort.isAsc
-            })
+            });
         }else{
             setSort({
                 column: label,
                 isAsc: false
-            })
+            });
         }
+
+        const sorted = sorting(label);
+        setSortedData(sorted);
     }
 
-    const minShow = currentPage === 1 ? 1 : (currentPage - 1) * entriesShown + 1;
-    const maxShow = currentPage * entriesShown < data.length ? currentPage * entriesShown : data.length ;
+    const sorting = (label) => {
+        const arraySort = sortedData.sort((a,b) => {
+
+            const labelA = a[label].trim().toLowerCase()
+            const labelB = b[label].trim().toLowerCase()
+
+            if (sort.isAsc) {
+                if(labelA > labelB) { return 1}
+                if(labelA < labelB) { return -1} 
+            } else {
+                if(labelA < labelB) { return 1}
+                if(labelA > labelB) { return -1} 
+            }
+
+            return 0
+        });
+
+        return arraySort
+    }
+
+    const minShow = currentPage === 1 
+        ? 1 
+        : (currentPage - 1) * entriesShown + 1;
+    const maxShow = currentPage * entriesShown < data.length 
+        ? currentPage * entriesShown 
+        : data.length ;
+    const minShowFiltered = currentPage === 1 
+        ? sortedData.length > 0 
+            ? 1
+            : 0
+        : (currentPage - 1) * entriesShown + 1;
+    const maxShowFiltered = currentPage * entriesShown < sortedData.length
+        ? currentPage * entriesShown
+        : sortedData.length;
     return (
         <div className="dataTable">
             <div className="top-table">
                 <LengthEntries handleChange={setEntriesShown} />
-                <Search />
+                <Search  data={data} handleSearchData={setSortedData} handleIsSearching={setIsSearching}/>
             </div>
 
-            <Table labels={labels} data={data} minShow={minShow} maxShow={maxShow} sort={sort} handleSort={handleSort}/>
+            <Table labels={labels} data={sortedData} minShow={minShow} maxShow={maxShow} sort={sort} handleSort={handleSort} sortedData={sortedData}/>
 
             <div className="bot-table">
-                <ShowEntries minShow={minShow} maxShow={maxShow} totalEntries={data.length}/>
+                <ShowEntries minShow={minShow} maxShow={maxShow} isSearching={isSearching} totalEntries={data.length} minShowFiltered={minShowFiltered} maxShowFiltered={maxShowFiltered}/>
                 <Pagination currentPage={currentPage} handleClick={setCurrentPage} totalEntries={data.length} entriesShown={entriesShown}/>
             </div>
         </div>
