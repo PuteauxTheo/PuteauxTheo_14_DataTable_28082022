dcd9f52e79445d9b9af236cee35fa0962abf0600
Theo Puteaux <theo.ptx@laposte.net>
2022-09-14 13:44:59 +0200

    fix search input

--- src/DataTables/Search.js
+++ src/DataTables/Search.js
@@ -1,9 +1,27 @@
-export default function Search(){
+export default function Search({ data, handleSearchData, handleIsSearching }){
+
+    const handleSearch = (e) => {
+        const input = e.target.value.trim().toLowerCase()
+        if (input.length > 0) {
+            const dataFilter = data.filter((el) => {
+                const values = Object.values(el).map((value) => value.trim().toLowerCase()).join(" ")
+                return values.includes(input)
+            })
+            handleIsSearching(true)
+            handleSearchData(dataFilter)
+            
+
+        } else {
+            handleIsSearching(false)
+            handleSearchData(data)
+        }
+    }
+
     return (
         <div className="dataTable-search">
             <label>
                 <p>Search</p>
-                <input type="search" />
+                <input type="search" onChange={(e) => handleSearch(e)}/>
             </label>
         </div>
     )
